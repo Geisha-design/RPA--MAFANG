621779381c4350a9d5a98c3b1c41756e0e367ab1
齐雨争 <qiyz@smartebao.com>
2025-09-18 11:44:39 +0800

    vv version

--- mafang_login.py
+++ mafang_login.py
@@ -8,18 +8,17 @@ from datetime import datetime
 import concurrent.futures
 from threading import Lock
 import html
-
 import AOSCCOCR
 
 
 def randomSleep():
-    # 设置随机休眠时间，范围从min_seconds到max_seconds，整数秒
+    # Set random sleep time, range from min_seconds to max_seconds, integer seconds
     min_seconds = 1
     max_seconds = 3
-    # 生成一个随机整数，表示休眠的时间（秒）
+    # Generate a random integer representing sleep time (seconds)
     sleep_time = random.randint(min_seconds, max_seconds)
     print(f"Randomly selected sleep time: {sleep_time} seconds")
-    # 让程序休眠这个随机时间
+    # Let the program sleep for this random time
     time.sleep(sleep_time)
 def rpapageshadow():
     co = ChromiumOptions()
@@ -29,12 +28,11 @@ def rpapageshadow():
     page = WebPage(chromium_options=co, session_or_options=so)
     page.get('https://www.nbsinglewindow.cn/user/login.do')
     logger.info('第一次cookie状态检测')
-    # cookiea = page.cookies(as_dict=True)  xpath://*[@id="cvf-page-content"]/div/div/div/div[2]/div/img
     cookiea = page.cookies()
     dictionary = {cookie['name']: cookie['value'] for cookie in cookiea}
     cookiea = dictionary
     logger.info(cookiea)
-    logger.info('登录标识状态A')
+    logger.info('Login status A')
     logger.info(cookiea.get('eporttoken'))
     if cookiea.get('eporttoken') is None:
         page.ele('xpath://*[@id="userId"]').input('nbfar')
@@ -42,37 +40,40 @@ def rpapageshadow():
         page.ele('xpath://*[@id="userPassword"]').input('nbfar666')
         randomSleep()
 
-        # 调用 AOSCC 的OCR识别模式 开始
+        # Call AOSCC OCR recognition mode start
         page.ele('xpath://*[@id="certificationCodeImg"]').get_screenshot('thescreen.png')
         randomSleep()
-        # OCR识别模块
+        # OCR recognition module
         with open("thescreen.png", 'rb') as f:
             img_bytes = f.read()
         ocr = AOSCCOCR.AosccOcr()
         poses = ocr.classification(img_bytes)
         logger.info(poses)
-        # 调用 AOSCC 的OCR识别模式 结束
-
-
+        # Call AOSCC OCR recognition mode end
 
         page.ele('xpath://*[@id="certificationCode"]').input(poses)
-        # 这个按钮标签为了稳定安全考虑 后续更换为点控式按钮
+        # This button tag is for stability and security considerations, will be replaced with point-control button later
         randomSleep()
         page.ele('xpath://html/body/div[2]/div[2]/a[1]').click()
         randomSleep()
 
-    logger.info('第二次cookie状态检测')
+    logger.info('Second cookie status detection')
     cookieb = page.cookies()
     dictionary = {cookie['name']: cookie['value'] for cookie in cookieb}
     cookieb = dictionary
     logger.info(cookieb)
-    logger.info('登录标识状态B  ')
+    logger.info('Login status B')
     logger.info(cookieb.get('eporttoken'))
-
 
 
     page.get('https://www.nbsinglewindow.cn/user/login.do?redirectURL=http%3A%2F%2Fp.nbsinglewindow.cn%2Fmember%2Freleaseinfo%2Freleaseinfo%21allquery.do')
     return page
+
+
+
+
+def fetch_bill_of_lading_data():
+    pass
 
 
 
